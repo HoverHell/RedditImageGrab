fb17202b59e7212bc0822f5346e7e7f73db78148
Daniel <Daniel@DanielsMacBookPro.local>
2011-11-06 19:09:22 -0800

    Added handling for content-type

--- redditdownload.py
+++ redditdownload.py
@@ -9,11 +9,10 @@ from os import mkdir
 if __name__ == "__main__": 
     PARSER = ArgumentParser( description='Downloads files with specified externsion from the specified subreddit.')
     PARSER.add_argument( 'reddit', metavar='r', help='Subreddit name.')
-    PARSER.add_argument( 'extension', metavar='e', help='File extension of interest.')
     PARSER.add_argument( 'dir', metavar='d', help='Dir to put downloaded files in.')
     ARGS = PARSER.parse_args()
  
-    print 'Downloading items with exentions "%s" from "%s" subreddit' % (ARGS.extension, ARGS.reddit)
+    print 'Downloading images from "%s" subreddit' % (ARGS.reddit)
     
     JSON = urlopen( 'http://www.reddit.com/r/%s.json' % ARGS.reddit ).read()
     DECODER = JSONDecoder()
@@ -28,22 +27,36 @@ if __name__ == "__main__":
     N = len( ITEMS )
     D = E = S = 0
     for ITEM in ITEMS:
-        # Only try to download files with extensions we are interested in.
-        if ITEM['url'].endswith( ARGS.extension ):
-            FILENAME = pathjoin( ARGS.dir, '%s.%s' % (ITEM['id'], ARGS.extension) )
-            # Don't download files multiple times!
-            if not pathexists( FILENAME ): 
-                print '\tDownloading %s to %s.' % (ITEM['url'], FILENAME)
-                RESPONSE = urlopen( ITEM['url'] )
+        FILENAME = pathjoin( ARGS.dir, '%s.jpg' % (ITEM['id'] ) )
+        # Don't download files multiple times!
+        if not pathexists( FILENAME ): 
+            RESPONSE = urlopen( ITEM['url'] )
+            INFO = RESPONSE.info()
+            
+            # Work out file type either from the response or the url.
+            if 'content-type' in INFO.keys():
+                FILETYPE = INFO['content-type']
+            elif ITEM['url'].endswith( 'jpg' ):
+                FILETYPE = 'image/jpeg'
+            elif ITEM['url'].endswith( 'jpeg' ):
+                FILETYPE = 'image/jpeg'
+            else:
+                FILETYPE = 'unknown'
+                 
+            # Only try to download jpeg images.
+            if FILETYPE == 'image/jpeg':
                 FILEDATA = RESPONSE.read()
                 FILE = open( FILENAME, 'wb')
                 FILE.write(FILEDATA)
                 FILE.close()
+                print '\tDownloaded %s to %s.' % (ITEM['url'],FILENAME)
                 D += 1
             else:
-                E += 1
+                print '\tWRONG FILE TYPE: %s has type: %s!' % (ITEM['url'],INFO['content-type'])
+                S += 1
         else:
-            S += 1
+            print '\tALREADY EXISTS: %s for %s already exists.' % (FILENAME,ITEM['url'])
+            E += 1
 
     print 'Downloaded %d of %d (Skipped %d, Exists %d)' % (D, N, S, E)
         
