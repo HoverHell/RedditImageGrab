045fec794c66027081bdb33db0570f44cd50d892
Daniel <Daniel@DanielsMacBookPro.local>
2011-11-06 20:42:58 -0800

    Fix case of variable last.

--- redditdownload.py
+++ redditdownload.py
@@ -33,58 +33,58 @@ if __name__ == "__main__":
         mkdir( ARGS.dir )
 
     while len(ITEMS) > 0:
-        last = ''
+        LAST = ''
         N += len( ITEMS )
         for ITEM in ITEMS:
-          FILENAME = pathjoin( ARGS.dir, '%s.jpg' % (ITEM['id'] ) )
-          # Don't download files multiple times!
-          if not pathexists( FILENAME ):
-              try:
-                  if 'imgur.com' in ITEM['url']:
-                      # Change .png to .jpg for imgur urls. 
-                      if ITEM['url'].endswith('.png'):
-                          ITEM['url'] = ITEM['url'].replace('.png','.jpg')
-                      # Add .jpg to imgur urls that are missing it.
-                      elif '.jpg' not in ITEM['url']:
-                          ITEM['url'] = '%s.jpg' % ITEM['url']
-                      elif '.jpeg' not in ITEM['url']:
-                          ITEM['url'] = '%s.jpg' % ITEM['url']
+            FILENAME = pathjoin( ARGS.dir, '%s.jpg' % (ITEM['id'] ) )
+            # Don't download files multiple times!
+            if not pathexists( FILENAME ):
+                try:
+                    if 'imgur.com' in ITEM['url']:
+                        # Change .png to .jpg for imgur urls. 
+                        if ITEM['url'].endswith('.png'):
+                            ITEM['url'] = ITEM['url'].replace('.png','.jpg')
+                        # Add .jpg to imgur urls that are missing it.
+                        elif '.jpg' not in ITEM['url']:
+                            ITEM['url'] = '%s.jpg' % ITEM['url']
+                        elif '.jpeg' not in ITEM['url']:
+                            ITEM['url'] = '%s.jpg' % ITEM['url']
 
-                  RESPONSE = urlopen( ITEM['url'] )
-                  INFO = RESPONSE.info()
-                  
-                  # Work out file type either from the response or the url.
-                  if 'content-type' in INFO.keys():
-                      FILETYPE = INFO['content-type']
-                  elif ITEM['url'].endswith( 'jpg' ):
-                      FILETYPE = 'image/jpeg'
-                  elif ITEM['url'].endswith( 'jpeg' ):
-                      FILETYPE = 'image/jpeg'
-                  else:
-                      FILETYPE = 'unknown'
-                       
-                  # Only try to download jpeg images.
-                  if FILETYPE == 'image/jpeg':
-                      FILEDATA = RESPONSE.read()
-                      FILE = open( FILENAME, 'wb')
-                      FILE.write(FILEDATA)
-                      FILE.close()
-                      print '\tDownloaded %s to %s.' % (ITEM['url'],FILENAME)
-                      D += 1
-                  else:
-                      print '\tWRONG FILE TYPE: %s has type: %s!' % (ITEM['url'],INFO['content-type'])
-                      S += 1
-              except HTTPError as ERROR:
-                      print '\tHTTP ERROR: Code %s for %s.' % (ERROR.code,ITEM['url'])
-                      F += 1
-              except URLError as ERROR:
-                      print '\tURL ERROR: %s!' % ITEM['url']
-                      F += 1
-          else:
-              print '\tALREADY EXISTS: %s for %s already exists.' % (FILENAME,ITEM['url'])
-              E += 1
-          last = ITEM['id']
-          ITEMS = getitems( ARGS.reddit, last )
+                    RESPONSE = urlopen( ITEM['url'] )
+                    INFO = RESPONSE.info()
+                    
+                    # Work out file type either from the response or the url.
+                    if 'content-type' in INFO.keys():
+                        FILETYPE = INFO['content-type']
+                    elif ITEM['url'].endswith( 'jpg' ):
+                        FILETYPE = 'image/jpeg'
+                    elif ITEM['url'].endswith( 'jpeg' ):
+                        FILETYPE = 'image/jpeg'
+                    else:
+                        FILETYPE = 'unknown'
+                         
+                    # Only try to download jpeg images.
+                    if FILETYPE == 'image/jpeg':
+                        FILEDATA = RESPONSE.read()
+                        FILE = open( FILENAME, 'wb')
+                        FILE.write(FILEDATA)
+                        FILE.close()
+                        print '\tDownloaded %s to %s.' % (ITEM['url'],FILENAME)
+                        D += 1
+                    else:
+                        print '\tWRONG FILE TYPE: %s has type: %s!' % (ITEM['url'],INFO['content-type'])
+                        S += 1
+                except HTTPError as ERROR:
+                        print '\tHTTP ERROR: Code %s for %s.' % (ERROR.code,ITEM['url'])
+                        F += 1
+                except URLError as ERROR:
+                        print '\tURL ERROR: %s!' % ITEM['url']
+                        F += 1
+            else:
+                print '\tALREADY EXISTS: %s for %s already exists.' % (FILENAME,ITEM['url'])
+                E += 1
+            LAST = ITEM['id']
+        ITEMS = getitems( ARGS.reddit, LAST )
 
     print 'Downloaded %d of %d (Skipped %d, Exists %d)' % (D, N, S, E)
         
