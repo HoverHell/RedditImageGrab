cc841f88653b2c20d568d1d7ae81d061e881536f
Mike Frisch <mfrisch@rogers.com>
2012-01-01 16:38:50 -0500

    Improved compliance with PEP 8 style guide

--- reddit.py
+++ reddit.py
@@ -1,18 +1,19 @@
 """Return list of items from a sub-reddit of reddit.com."""
 
-from urllib2 import urlopen, HTTPError 
+from urllib2 import urlopen, HTTPError
 from json import JSONDecoder
 
-def getitems( subreddit, previd=''):
+
+def getitems(subreddit, previd=''):
     """Return list of items from a subreddit."""
     url = 'http://www.reddit.com/r/%s.json' % subreddit
     # Get items after item with 'id' of previd.
     if previd != '':
         url = '%s?after=t3_%s' % (url, previd)
     try:
-        json = urlopen( url ).read()
-        data = JSONDecoder().decode( json )
-        items = [ x['data'] for x in data['data']['children'] ]
+        json = urlopen(url).read()
+        data = JSONDecoder().decode(json)
+        items = [x['data'] for x in data['data']['children']]
     except HTTPError as ERROR:
         print '\tHTTP ERROR: Code %s for %s.' % (ERROR.code, url)
         items = []
@@ -21,11 +22,11 @@ def getitems( subreddit, previd=''):
 if __name__ == "__main__":
 
     print 'Recent items for Python.'
-    ITEMS = getitems( 'python' )
+    ITEMS = getitems('python')
     for ITEM in ITEMS:
         print '\t%s - %s' % (ITEM['title'], ITEM['url'])
 
     print 'Previous items for Python.'
-    OLDITEMS = getitems( 'python', ITEMS[-1]['id'] )
+    OLDITEMS = getitems('python', ITEMS[-1]['id'])
     for ITEM in OLDITEMS:
         print '\t%s - %s' % (ITEM['title'], ITEM['url'])
