b2703f2ec4879d2a652a0919639381720bc26d30
Daniel Brown <github@endlesslycurious.com>
2012-01-15 20:04:15 -0800

    Make Pylint happy

--- redditdownload.py
+++ redditdownload.py
@@ -18,7 +18,7 @@ class FileExistsException(Exception):
     """Exception raised when file exists in specified directory"""
 
 
-def _extractImgurAlbumUrls(albumUrl):
+def extract_imgur_album_urls(album_url):
     """
     Given an imgur album URL, attempt to extract the images within that
     album
@@ -26,7 +26,7 @@ def _extractImgurAlbumUrls(albumUrl):
     Returns:
         List of qualified imgur URLs
     """
-    response = urlopen(albumUrl)
+    response = urlopen(album_url)
     info = response.info()
 
     # Rudimentary check to ensure the URL actually specifies an HTML file
@@ -35,29 +35,29 @@ def _extractImgurAlbumUrls(albumUrl):
 
     filedata = response.read()
 
-    m = re.compile(r'\"hash\":\"(.[^\"]*)\"')
+    match = re.compile(r'\"hash\":\"(.[^\"]*)\"')
 
     items = []
 
-    fp = StringIO.StringIO(filedata)
+    memfile = StringIO.StringIO(filedata)
 
-    for line in fp.readlines():
-        results = re.findall(m, line)
+    for line in memfile.readlines():
+        results = re.findall(match, line)
         if not results:
             continue
 
         items += results
 
-    fp.close()
+    memfile.close()
 
-    urls = ['http://i.imgur.com/%s.jpg' % (hash) for hash in items]
+    urls = ['http://i.imgur.com/%s.jpg' % (imghash) for imghash in items]
 
     return urls
 
 
-def _downloadFromUrl(url, destFile):
+def download_from_url(url, dest_file):
     """
-    Attempt to download file specified by url to 'destFile'
+    Attempt to download file specified by url to 'dest_file'
 
     Raises:
         WrongFileTypeException
@@ -71,35 +71,35 @@ def _downloadFromUrl(url, destFile):
             the destination directory.
     """
     # Don't download files multiple times!
-    if pathexists(destFile):
-        raise FileExistsException('URL [%s] already downloaded to %s.' % (url,destFile))
+    if pathexists(dest_file):
+        raise FileExistsException('URL [%s] already downloaded.' % url)
 
-    RESPONSE = urlopen(url)
-    INFO = RESPONSE.info()
+    response = urlopen(url)
+    info = response.info()
 
     # Work out file type either from the response or the url.
-    if 'content-type' in INFO.keys():
-        FILETYPE = INFO['content-type']
+    if 'content-type' in info.keys():
+        filetype = info['content-type']
     elif url.endswith('.jpg') or url.endswith('.jpeg'):
-        FILETYPE = 'image/jpeg'
+        filetype = 'image/jpeg'
     elif url.endswith('.png'):
-        FILETYPE = 'image/png'
+        filetype = 'image/png'
     elif url.endswith('.gif'):
-        FILETYPE = 'image/gif'
+        filetype = 'image/gif'
     else:
-        FILETYPE = 'unknown'
+        filetype = 'unknown'
 
     # Only try to download acceptable image types
-    if not FILETYPE in ['image/jpeg', 'image/png', 'image/gif']:
-        raise WrongFileTypeException('WRONG FILE TYPE: %s has type: %s!' % (url, FILETYPE))
+    if not filetype in ['image/jpeg', 'image/png', 'image/gif']:
+        raise WrongFileTypeException('WRONG FILE TYPE: %s has type: %s!' % (url, filetype))
 
-    FILEDATA = RESPONSE.read()
-    FILE = open(destFile, 'wb')
-    FILE.write(FILEDATA)
-    FILE.close()
+    filedata = response.read()
+    filehandle = open(dest_file, 'wb')
+    filehandle.write(filedata)
+    filehandle.close()
 
 
-def _processImgurUrl(url):
+def process_imgur_url(url):
     """
     Given an imgur URL, determine if it's a direct link to an image or an
     album.  If the latter, attempt to determine all images within the album
@@ -108,14 +108,14 @@ def _processImgurUrl(url):
         list of imgur URLs
     """
     if 'imgur.com/a/' in url:
-        return _extractImgurAlbumUrls(url)
+        return extract_imgur_album_urls(url)
 
     # Change .png to .jpg for imgur urls.
     if url.endswith('.png'):
         url = url.replace('.png', '.jpg')
     else:
         # Extract the file extension
-        basename, ext = pathsplitext(pathbasename(url))
+        ext = pathsplitext(pathbasename(url))[1]
         if not ext:
             # Append a default
             url += '.jpg'
@@ -123,20 +123,27 @@ def _processImgurUrl(url):
     return [url]
 
 
-def _extractUrls(url):
+def extract_urls(url):
+    """
+    Given an URL checks to see if its an imgur.com URL, handles imgur hosted
+    images if present as single image or image album.
+
+    Returns:
+        list of image urls.
+    """
     urls = []
 
-    if 'imgur.com' in ITEM['url']:
-        urls = _processImgurUrl(ITEM['url'])
+    if 'imgur.com' in url:
+        urls = process_imgur_url(url)
     else:
-        urls = [ITEM['url']]
+        urls = [url]
 
     return urls
 
 if __name__ == "__main__":
     PARSER = ArgumentParser(description='Downloads files with specified extension from the specified subreddit.')
     PARSER.add_argument('reddit', metavar='<subreddit>', help='Subreddit name.')
-    PARSER.add_argument('dir', metavar='<destFile>', help='Dir to put downloaded files in.')
+    PARSER.add_argument('dir', metavar='<dest_file>', help='Dir to put downloaded files in.')
     PARSER.add_argument('-last', metavar='l', default='', required=False, help='ID of the last downloaded file.')
     PARSER.add_argument('-score', metavar='s', default=0, type=int, required=False, help='Minimum score of images to download.')
     PARSER.add_argument('-num', metavar='n', default=0, type=int, required=False, help='Number of images to download.')
@@ -149,7 +156,7 @@ if __name__ == "__main__":
 
     print 'Downloading images from "%s" subreddit' % (ARGS.reddit)
 
-    nTotal = nDownloaded = nErrors = nSkipped = nFailed = 0
+    TOTAL = DOWNLOADED = ERRORS = SKIPPED = FAILED = 0
     FINISHED = False
 
     # Create the specified directory if it doesn't already exist.
@@ -157,9 +164,9 @@ if __name__ == "__main__":
         mkdir(ARGS.dir)
 
     # If a regex has been specified, compile the rule (once)
-    reRule = None
+    RE_RULE = None
     if ARGS.regex:
-        reRule = re.compile(ARGS.regex)
+        RE_RULE = re.compile(ARGS.regex)
 
     LAST = ARGS.last
 
@@ -170,79 +177,81 @@ if __name__ == "__main__":
             break
 
         for ITEM in ITEMS:
-            nTotal += 1
+            TOTAL += 1
 
             if ITEM['score'] < ARGS.score:
                 if ARGS.verbose:
                     print '    SCORE: %s has score of %s which is lower than required score of %s.' % (ITEM['id'], ITEM['score'], ARGS.score)
 
-                nSkipped += 1
+                SKIPPED += 1
                 continue
             elif ARGS.sfw and ITEM['over_18']:
                 if ARGS.verbose:
                     print '    NSFW: %s is marked as NSFW.' % (ITEM['id'])
 
-                nSkipped += 1
+                SKIPPED += 1
                 continue
             elif ARGS.nsfw and not ITEM['over_18']:
                 if ARGS.verbose:
                     print '    Not NSFW, skipping %s' % (ITEM['id'])
 
-                nSkipped += 1
+                SKIPPED += 1
                 continue
-            elif ARGS.regex and not re.match(reRule, ITEM['title']):
+            elif ARGS.regex and not re.match(RE_RULE, ITEM['title']):
                 if ARGS.verbose:
                     print '    Regex match failed'
 
-                nSkipped += 1
+                SKIPPED += 1
                 continue
 
             FILECOUNT = 0
-            URLS = _extractUrls(ITEM['url'])
-            for url in URLS:
+            URLS = extract_urls(ITEM['url'])
+            for URL in URLS:
                 try:
-                    FILEEXT = pathsplitext(url)[1]
                     # Trim any http query off end of file extension.
+                    FILEEXT = pathsplitext(URL)[1]
                     if '?' in FILEEXT:
                         FILEEXT = FILEEXT[:FILEEXT.index('?')]
+
                     # Only append numbers if more than one file.
                     FILENUM = ('_%d' % FILECOUNT if len(URLS) > 1 else '')
                     FILENAME = '%s%s%s' % (ITEM['id'], FILENUM, FILEEXT)
                     FILEPATH = pathjoin(ARGS.dir, FILENAME) 
+
                     # Download the image
-                    _downloadFromUrl(url, FILEPATH)
+                    download_from_url(URL, FILEPATH)
 
                     # Image downloaded successfully!
-                    print '    Downloaded URL [%s].' % (url)
-                    nDownloaded += 1
+                    print '    Downloaded URL [%s] as [%s].' % (URL, FILENAME)
+                    DOWNLOADED += 1
                     FILECOUNT += 1
 
-                    if ARGS.num > 0 and nDownloaded >= ARGS.num:
+                    if ARGS.num > 0 and DOWNLOADED >= ARGS.num:
                         FINISHED = True
                         break
                 except WrongFileTypeException as ERROR:
                     print '    %s' % (ERROR)
-                    nSkipped += 1
+                    SKIPPED += 1
                 except FileExistsException as ERROR:
                     print '    %s' % (ERROR)
-                    nErrors += 1
+                    ERRORS += 1
                     if ARGS.update:
                         print '    Update complete, exiting.'
                         FINISHED = True
                         break
                 except HTTPError as ERROR:
-                    print '    HTTP ERROR: Code %s for %s.' % (ERROR.code, url)
-                    nFailed += 1
+                    print '    HTTP ERROR: Code %s for %s.' % (ERROR.code, URL)
+                    FAILED += 1
                 except URLError as ERROR:
-                    print '    URL ERROR: %s!' % (url)
-                    nFailed += 1
+                    print '    URL ERROR: %s!' % (URL)
+                    FAILED += 1
                 except InvalidURL as ERROR:
-                    print '    Invalid URL: %s!' % (url)
-                    nFailed += 1
+                    print '    Invalid URL: %s!' % (URL)
+                    FAILED += 1
 
             if FINISHED:
                 break
 
         LAST = ITEM['id']
 
-    print 'Downloaded %d files (Processed %d, Skipped %d, Exists %d)' % (nDownloaded, nTotal, nSkipped, nErrors)
+    print 'Downloaded %d files (Processed %d, Skipped %d, Exists %d)' % (DOWNLOADED, TOTAL, SKIPPED, ERRORS)
