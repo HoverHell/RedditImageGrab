4c61ad10b13d2136a5e2116e136fe90ee21eb562
rachmadani haryono <foreturiga@gmail.com>
2015-07-31 16:20:36 +0800

    use setuptools instead of distutil for setup

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from os import path, getcwd
+from setuptools import setup, find_packages
+from apt.package import Package
+from IPython.core.release import long_description
+
+with open(path.join(getcwd(), 'readme.md')) as f:
+    long_description = f.read()
+
 
 setup(name='RedditImageGrab',
       version='0.1',
       description='Downloads JPEG images from sub-reddits of reddit.com',
+      long_description=long_description,
+      packages=find_packages(),
+      package_data = {'': ['*.md']}, 
+
+      # metadata for upload to PyPI
       author='HoverHell',
       author_email='hoverhell@gmail.com',
+      keywords="reddit image downloader",
       url='https://github.com/HoverHell/RedditImageGrab',
-      packages=['img_scrap_stuff', 'reddit', 'redditdownload', 'scrap_wrongies'],
+
+
      )
