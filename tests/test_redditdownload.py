c34a9e549d7a7f92fdb41e7cc9990588aea23fe6
rachmadaniHaryono <foreturiga@gmail.com>
2016-10-03 22:55:30 +0800

    new: test for download_from_url func.

--- tests/test_redditdownload.py
+++ tests/test_redditdownload.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import unittest
+from os import (
+    getcwd,
+    path,
+)
 from unittest import TestCase
-from os import getcwd
+import unittest
+try:  # py3
+    from unittest import mock
+except ImportError:  # py2
+    import mock
 
-from redditdownload import parse_args, process_deviant_url, process_imgur_url
+import pytest
+
+from redditdownload import (
+    download_from_url,
+    FileExistsException,
+    parse_args,
+    process_deviant_url,
+    process_imgur_url,
+    WrongFileTypeException,
+)
 
 
 class TestParseArgs(TestCase):
@@ -59,6 +75,78 @@ class TestProcessImgurUrl(TestCase):
         self.assertIsInstance(result, list)
         self.assertEqual(len(result), 1)
         self.assertIn(self.single_url, result)
+
+class TestDownloadFromUrl(TestCase):
+    """test download_from_url func."""
+
+    def test_empty_mock_input(self):
+        """test with empty mock input."""
+        with pytest.raises(TypeError):
+            download_from_url(mock.Mock(), mock.Mock())
+
+    @mock.patch('redditdownload.redditdownload.pathexists')
+    def test_empty_mock_input_with_patched_pathexist(self, mock_pathexists):
+        with pytest.raises(FileExistsException):
+            download_from_url(mock.Mock(), mock.Mock())
+
+        mock_pathexists.return_value = False
+
+        with pytest.raises(TypeError):
+            download_from_url(mock.Mock(), mock.Mock())
+
+    @mock.patch('redditdownload.redditdownload.pathexists')
+    def test_imgur_url_with_existing_alt_ext_file(self, mock_pathexists):
+        """test imgur.com url when alternative ext file already exist in file."""
+        imgur_url = 'https://i.imgur.com/3panxHg.jpg'
+        dest_file = '3panxHg.jpg'
+        for ext in ['.jpg', '.jpeg']:
+            dest_file = path.splitext(path.basename(imgur_url))[0] + ext
+            mock_pathexists.side_effect = lambda x: False if x == dest_file else True
+            with pytest.raises(FileExistsException):
+                download_from_url(imgur_url, dest_file)
+
+    @mock.patch('redditdownload.redditdownload.pathexists')
+    def test_unknown_filetype_url(self, mock_pathexists):
+        """test url with unknown_filetype."""
+        url = 'randomwebsite.com/random_path'
+        dest_file = path.basename(url)
+        mock_pathexists.return_value = False
+        # ValueError raised because random url
+        with pytest.raises(ValueError):
+            download_from_url(url, dest_file)
+
+        with mock.patch('redditdownload.redditdownload.request') as mock_request:
+            with pytest.raises(WrongFileTypeException):
+                download_from_url(url, dest_file)
+            mock_request.assert_any_call(url)
+
+            call_list = [
+                mock.call().info(),
+                mock.call().info().keys(),
+                # NOTE:dunder not recognized by mock
+                # mock.call().info().keys().__contains__('content-type'),
+            ]
+            for single_call in call_list:
+                assert single_call in mock_request.mock_calls
+
+    @mock.patch('redditdownload.redditdownload.request')
+    @mock.patch('redditdownload.redditdownload.pathexists')
+    def test_working_url(self, mock_pathexists, mock_request):
+        """test working url."""
+        valid_ext = '.jpg'
+        url = 'randomwebsite.com/random_path' + valid_ext
+        dest_file = path.basename(url)
+        mock_pathexists.return_value = False
+
+        mock_open_target = 'redditdownload.redditdownload.open'
+        with mock.patch(mock_open_target, mock.mock_open(), create=True) as dfu_mock_open:
+            download_from_url(url, dest_file)
+            # test mock_request
+            assert mock.call().read() in mock_request.mock_calls
+            # test dfu_mock_open
+            dfu_mock_open.assert_any_call(dest_file, 'wb')
+            assert mock.call().write(mock_request(url).read()) in dfu_mock_open.mock_calls
+            assert mock.call().close() in dfu_mock_open.mock_calls
 
 if __name__ == '__main__':
     unittest.main()
